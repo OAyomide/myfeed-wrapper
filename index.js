ca911c945fc9cdd1efee7b5f9c223bc983b5e982
Ayomide <onigbindeayomide@gmail.com>
2018-12-27 06:52:21 +0100

    return all player scores

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ class Wrapper {
     super()
   }
 
+  async playerStats(name, options) {
+    try {
+      const { season, date, team, rosterstatus, player, position, country, sort, offset, limit, force, format } = options
+      const fmt = format || 'json'
+      const request = await axios({
+        method: 'get',
+        url: `https://api.mysportsfeeds.com/v2.0/pull/nhl/players.${fmt}`
+      })
+      return request.data
+    } catch (e) {
+      console.log(`ERROR FINDING THE PLAYER SCORES`)
+    }
+  }
+
 
   //encoding encodes the user token as per the docs, to make the request tot eh endpoint
   encode() {
